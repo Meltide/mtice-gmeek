d5eb2898414cc66aa66fcf3116d2abb58a754422
MeltIce2024 <114905314+MeltIce2024@users.noreply.github.com>
2024-10-01 10:38:16 +0800

    Add files via upload

--- static/RonanTheme.js
+++ static/RonanTheme.js
@@ -37,7 +37,7 @@ document.addEventListener('DOMContentLoaded', function() {
         }
 
         html {    
-            background: url('https://blog.freeblock.cn/background.webp') no-repeat center center fixed;
+            background: url('https://www.freeimg.cn/i/2024/10/01/66fb52433c7ed.webp') no-repeat center center fixed;
             background-size: cover;
         }
 
@@ -162,7 +162,7 @@ document.addEventListener('DOMContentLoaded', function() {
         style.innerHTML = `
         
         html {    
-            background: url('https://blog.freeblock.cn/background.webp') no-repeat center center fixed;
+            background: url('https://www.freeimg.cn/i/2024/10/01/66fb52433c7ed.webp') no-repeat center center fixed;
             background-size: cover;
         }
 
